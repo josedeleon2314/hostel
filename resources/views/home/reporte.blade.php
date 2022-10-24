c462bfdf50112c5a8305aced43c947986163bc2f
jose de leon <trabajosjose0@gmail.com>
2022-10-24 11:38:47 -0500

    5 commit registrar modificacion del front

--- resources/views/home/reporte.blade.php
+++ resources/views/home/reporte.blade.php
@@ -4,7 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Reporte</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.21.1/dist/bootstrap-table.min.css">
 </head>
 <body>
 @auth
@@ -12,7 +16,7 @@
 <br><br>
   <div class="container">
     
-    <table  class="table table-bordered table-striped table-hover" id="venta">
+    <table  class="table table-dark" id="venta">
       <thead>
         <tr class="text-center">
           <th width="5%">
